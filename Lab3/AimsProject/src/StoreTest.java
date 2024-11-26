5ca2200898a6d6ffeda3bcb6a6f8dc4ad310448e
nghiaduong <duongnghia2904204@gmail.com>
2024-11-26 15:49:27 +0700

    store

---
+++ Lab3/AimsProject/src/StoreTest.java
@@ -0,0 +1,19 @@
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.store.Store;
+
+public class StoreTest {
+	public static void main(String[] args) {
+		Store st= new Store();
+		for(int i=0; i<10; i++) {
+			DigitalVideoDisc d= new DigitalVideoDisc("dvd"+i);
+			st.addDVD(d);
+		}
+		
+		st.removeDVD(new DigitalVideoDisc("dvd4"));
+		
+		
+		//Check so luong xem da add or remove chua
+		System.out.println(st.getNumberOfDisc());
+	}
+}
